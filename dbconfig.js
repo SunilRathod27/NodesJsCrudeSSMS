2cf709bd044bd65dea50590b7a4d801c3c44d99b
unknown <shrathod515@gmail.com>
2021-10-12 18:41:43 +0530

    first commit

---
+++ dbconfig.js
@@ -0,0 +1,30 @@
+
+// const config = {
+//     user :'foo',
+//     password :'foo',
+//     server:'127.0.0.1',
+//     database:'Products',
+//     options:{
+//         trustedconnection: true,
+//         enableArithAbort : true, 
+//         instancename :'SQLEXPRESS'
+//     },
+//     port : 55892
+// }
+
+// module.exports = config; 
+
+const config = {
+    user :'Sunil',
+    password :'root',
+    server:'localhost',
+    database:'Products',
+    options:{
+        trustedconnection: true,
+        enableArithAbort : true, 
+        instancename :'SQLSERVER    '
+    },
+    port : 1433
+}
+
+module.exports = config; 
